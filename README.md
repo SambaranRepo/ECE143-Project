aa942ea5d7c092956a7a70049e5d4d99ba9a3102
SambaranRepo <sambaranghosal@gmail.com>
2021-11-24 20:46:45 -0800

    November 25 update. Instructions in README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ECE143-Project
+
+To-do November 25th
+a) All conferences include 2021 with affiliations (code : nips2.py , icml.py, iclr_2.py) (Sambaran, Sananya, Debaditya)
+b) Make the CVPR dataset same as our dataset (Rahul)
+c) Run the cite2.py code for cvpr, icml, iclr to get citations (create seperate files to store the citations in format : Year , Paper , Citations) (Sambaran, Sananya, Debaditya)
+
+To run cite2.py : 
+Go to https://www.scraperapi.com/
+Free Signup using gmail 
+Copy API Key to the code and place the key in line11 and line43 of cite2.py
--- src/icml.py
+++ src/icml.py
@@ -3,30 +3,41 @@ import requests
 import csv
 from tqdm import tqdm
 
-icml_links = ['https://icml.cc/Conferences/2018/Schedule?type=Poster','https://icml.cc/Conferences/2019/Schedule?type=Poster','https://icml.cc/Conferences/2020/Schedule?type=Poster']
+icml_links = ['https://icml.cc/Conferences/2016/Schedule?type=Poster','https://icml.cc/Conferences/2017/Schedule?type=Poster','https://icml.cc/Conferences/2018/Schedule?type=Poster','https://icml.cc/Conferences/2019/Schedule?type=Poster','https://icml.cc/Conferences/2020/Schedule?type=Poster']
 
 parent = '/home/sambaran/UCSD/Quarter2/ECE143/Project/data'
 csv_file = open(parent + '/icml_2018-2020.csv', 'w')
 csv_writer = csv.writer(csv_file)
-csv_writer.writerow(['YEAR', 'TITLE', 'Authors', 'Paper Link', 'ABSTRACT'])
+csv_writer.writerow(['YEAR', 'TITLE', 'Authors', 'Paper Link', 'ABSTRACT', 'Affiliations'])
 
 for icml in tqdm(icml_links):
     year = icml.split('/')[4]
     source = requests.get(icml).text
     soup = BeautifulSoup(source, 'lxml')
-    for section in tqdm(soup.find_all('div', class_='maincard narrower Poster')): 
+
+    for section in tqdm(soup.find_all('div', class_='maincard narrower poster')): 
         try:
             a= str(section).split('\n')
             id = a[0].split('card_')[1].split('">')[0]
             title = str(section.find('div',class_='maincardBody').text)
             authors = str(section.find('div',class_='maincardFooter').text)
             authors = authors.replace(' ·', ',')
-            link = 'https://icml.cc/Conferences/2020/Schedule?showEvent=' + id
-
+            link = f'https://icml.cc/Conferences/{year}/Schedule?showEvent=' + id
             pdf_source = requests.get(link).text
             pdf_soup = BeautifulSoup(pdf_source, 'lxml')
             abstract = pdf_soup.find('div', class_='abstractContainer').text
-            entry = [year,title, authors, link,abstract]
+            author_link = pdf_soup.find_all('button', class_ = 'btn btn-default')
+            affiliations = []
+            for item in author_link: 
+                author_id  = str(item).split("Speaker('")[1].split("')")[0]
+                # print(author_id)
+                auth_profile_link = f"https://icml.cc/Conferences/{year}/Schedule?showSpeaker={author_id}"
+                auth_page = requests.get(auth_profile_link).text
+                auth_soup = BeautifulSoup(auth_page, 'lxml')
+                name = auth_soup.find('div' , class_ = 'maincard Remark col-sm-12').h4.text
+                # print(name)
+                affiliations.append(name)
+            entry = [year,title, authors, link,abstract, affiliations]
         except Exception as e:
             entry = ''
         csv_writer.writerow(entry)
